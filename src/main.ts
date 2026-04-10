e543c2803b32cf1c44533ba3c9c745134b66a928
Arjuna Notanubun <arjuna_notanubun_33rpl@student.smktelkom-mlg.sch.id>
2026-04-10 07:18:22 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'; // Tambah ini [cite: 32]
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(); // Tambah ini untuk izinkan request dari frontend
 
   // Pakai ValidationPipe yang lama (jangan dihapus)
   app.useGlobalPipes(
@@ -15,16 +17,16 @@ async function bootstrap() {
     })
   );
 
-  // Konfigurasi Swagger sesuai Modul 7 [cite: 38, 39, 40, 41, 42]
+  // Konfigurasi Swagger sesuai Modul 7
   const config = new DocumentBuilder()
     .setTitle('Library API')
     .setDescription('Backend API Sistem Perpustakaan')
     .setVersion('1.0')
-    .addBearerAuth() // Penting untuk JWT [cite: 42, 51]
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document); // Akses di /api [cite: 45, 52, 53]
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
